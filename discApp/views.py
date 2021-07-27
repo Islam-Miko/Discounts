dfe0483002779db1458c349a1a25e7b8401c611e
Islam-Miko <islamimanhod@gmail.com>
2021-07-27 19:21:55 +0600

    filter_fields

--- discApp/views.py
+++ discApp/views.py
@@ -1,7 +1,7 @@
 
 from rest_framework import generics, views, pagination, filters
 from rest_framework.response import Response
-
+from django_filters.rest_framework import DjangoFilterBackend
 from discApp.models import Discount
 from . import serializers
 from .serializers import DiscountShortSerialzier
@@ -14,10 +14,12 @@ class ListDiscountApi(generics.ListAPIView):
     # def get_queryset(self):
     #     queryset = Discount.objects.filter(active=True)
     #     return queryset
-    queryset = Discount.objects.filter(active=True)
+    queryset = Discount.objects.filter()
     serializer_class = DiscountShortSerialzier
     pagination_class = pagination.LimitOffsetPagination
-    filter_backends = [filters.OrderingFilter,]
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter, DjangoFilterBackend]
+    filter_fields = ['active']
+    search_fields = ['category__type', 'description__description']
     ordering_fields =['company__address__city']
 
 
