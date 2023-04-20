bfb5139d0a0813b300202ecaf050a264fe408e80
miko <islamimanhod@gmail.com>
2023-04-20 17:24:55 +0600

    refactor: remove APScheduler, celery or huey will be used instead

--- discounts/apps.py
+++ discounts/apps.py
@@ -6,9 +6,3 @@ class DiscappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'discounts'
 
-    def ready(self):
-        print(
-            'Starting Scheduler'
-        )
-        from .scheuler import coupon_scheduler
-        coupon_scheduler.start()
--- discounts/scheuler/coupon_scheduler.py
+++
@@ -1,9 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from discounts.service import couponScheduler
-
-
-
-def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(couponScheduler, 'interval', minutes=1, id='apcheduler_1', replace_existing=True)
-    scheduler.start()
